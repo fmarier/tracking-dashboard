4ef41c568f0959e23041aa60934157d79b71dbb6
Francois Marier <francois@mozilla.com>
2015-08-11 15:19:14 -0700

    Update browser versions

--- dashboard.js
+++ dashboard.js
@@ -13,9 +13,9 @@ var endDate = new Date(2015, 0, 1);
 
 // Versions for which we have any data.
 var channels = {
-  nightly: [ "nightly/36", "nightly/37", "nightly/38", "nightly/39" ],
-  aurora: [ "aurora/35", "aurora/36", "aurora/37", "aurora/38", "aurora/39" ],
-  beta: [ "beta/35", "beta/36", "beta/37", "beta/38" ]
+  nightly: [ "nightly/40", "nightly/41", "nightly/42", "nightly/43" ],
+  aurora: [ "aurora/39", "aurora/40", "aurora/41", "aurora/42" ],
+  beta: [ "beta/38", "beta/39", "beta/40", "beta/41" ]
 };
 var currentChannel = "nightly";
 
